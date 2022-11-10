c200ec96c4a0e02481787aa3373700cbd0cd9377
mrbean <sam@you.com>
2022-11-09 23:45:53 -0500

    add simple write

--- src/main/scala/WeaviateDataWriter.scala
+++ src/main/scala/WeaviateDataWriter.scala
@@ -1,10 +1,34 @@
 package io.weaviate.spark
 
+
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.connector.write.{DataWriter, WriterCommitMessage}
+import technology.semi.weaviate.client.{Config, WeaviateClient}
+
+import java.util
+import scala.jdk.CollectionConverters._
 
 case class WeaviateDataWriter() extends DataWriter[InternalRow] with Serializable {
-  override def write(record: InternalRow): Unit = ???
+  val schema = Seq("title", "content")
+  override def write(record: InternalRow): Unit = {
+    val config = new Config("http", "localhost:8080")
+    val client = new WeaviateClient(config)
+
+    val properties = getPropertiesFromRecord(record)
+    val results = client
+      .data()
+      .creator()
+      .withProperties(properties)
+      .run()
+  }
+
+  private def getPropertiesFromRecord(record: InternalRow): util.Map[String, AnyRef] = {
+    val properties = scala.collection.mutable.Map[String, AnyRef]()
+    schema.zipWithIndex.foreach(itemWithIndex => properties(itemWithIndex._1) = record.getString(itemWithIndex._2))
+
+    properties.asJava
+  }
+
   override def close(): Unit = ???
   override def commit(): WriterCommitMessage = ???
   override def abort(): Unit = ???
