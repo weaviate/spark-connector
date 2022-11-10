d5ae5af5efa8605bc600ec1f2581ba55d82bdd62
mrbean <sam@you.com>
2022-11-09 21:20:07 -0500

    add capabilities impl

--- src/main/scala/WeaviateCluster.scala
+++ src/main/scala/WeaviateCluster.scala
@@ -4,11 +4,15 @@ import org.apache.spark.sql.connector.catalog.{SupportsWrite, TableCapability}
 import org.apache.spark.sql.connector.write.{LogicalWriteInfo, WriteBuilder}
 import org.apache.spark.sql.types.StructType
 
+import scala.jdk.CollectionConverters._
 import java.util
 
 case class WeaviateCluster() extends SupportsWrite {
   override def name(): String = ???
   override def schema(): StructType = ???
-  override def newWriteBuilder(info: LogicalWriteInfo): WriteBuilder = ???
-  override def capabilities(): util.Set[TableCapability] = ???
+  override def newWriteBuilder(info: LogicalWriteInfo): WriteBuilder = WeaviateWriteBuilder()
+  override def capabilities(): util.Set[TableCapability] = Set(
+    TableCapability.BATCH_WRITE
+  ).asJava
+
 }
