ca979b62b004592655434fa4531f609fcedb5f7e
Sam Stoelinga <sammiestoel@gmail.com>
2022-11-10 20:06:52 -0800

    Use weaviate with integration tests (#6)
    * Switch integration tests to use run_tests.sh
    
    * Exit run-tests.sh on failure and print commands
    
    * Fix GH action format for running script
    
    * Add weaviate container for testing
    
    * Update github action to use ubuntu 22.04

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,29 @@
 #!/usr/bin/env bash
 
+set -xe
+
+spark_container="spark-with-weaviate"
+weaviate_container="weaviate-test-will-be-removed"
+
+function cleanup {
+  set +e
+  echo "Deleting containers that were created for testing"
+  docker stop "$weaviate_container"
+  docker rm "$weaviate_container"
+  docker rm "$spark_container"
+}
+trap cleanup EXIT
+
 sbt assembly
 docker build -t spark-with-weaviate .
-docker run spark-with-weaviate /opt/spark/bin/spark-shell -i /opt/spark/example.scala
-
+docker run -d --name="$weaviate_container" \
+    -p 8080:8080 \
+    -e QUERY_DEFAULTS_LIMIT=25 \
+    -e AUTHENTICATION_ANONYMOUS_ACCESS_ENABLED='true' \
+    -e DEFAULT_VECTORIZER_MODULE='none' \
+    -e CLUSTER_HOSTNAME='node1' \
+    -e PERSISTENCE_DATA_PATH='./data' \
+    semitechnologies/weaviate:1.16.1
+# wait for weaviate to come up
+curl --retry-all-errors --retry-connrefused --retry 5 http://localhost:8080/v1/schema
+docker run --net=host --name "$spark_container" spark-with-weaviate /opt/spark/bin/spark-shell -i /opt/spark/example.scala
