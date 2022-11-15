cc4ee5d6f6b1b4ddea955736db8308f5b98c1780
Sam Stoelinga <sammiestoel@gmail.com>
2022-11-15 11:36:19 -0800

    Use commit coordinator to prevent speculative task

--- src/main/scala/WeaviateBatchWriter.scala
+++ src/main/scala/WeaviateBatchWriter.scala
@@ -7,6 +7,9 @@ case class WeaviateBatchWriter(weaviateOptions: WeaviateOptions, schema: StructT
   override def createBatchWriterFactory(info: PhysicalWriteInfo): DataWriterFactory = {
     WeaviateDataWriterFactory(weaviateOptions, schema)
   }
+
+  override def useCommitCoordinator(): Boolean = true
+
   override def abort(messages: Array[WriterCommitMessage]): Unit = {}
   override def commit(messages: Array[WriterCommitMessage]): Unit = {}
 }
