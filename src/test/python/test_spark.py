e3d76b727baf534a1689fb05cc0cc9ce40ebb602
Sam Stoelinga <sammiestoel@gmail.com>
2023-02-02 14:42:30 -0800

    Support null values for all Spark Datatypes

--- src/test/python/test_spark.py
+++ src/test/python/test_spark.py
@@ -6,7 +6,8 @@ import re
 from pyspark.sql import SparkSession
 import pytest
 import docker
-from pyspark.sql.types import StructType, StructField, StringType, ArrayType
+from pyspark.sql.types import StructType, StructField, StringType, ArrayType, DoubleType, BooleanType, IntegerType, \
+    DateType
 import weaviate
 
 
@@ -80,7 +81,7 @@ def test_string_arrays(spark: SparkSession, weaviate_client: weaviate.Client):
         StructField('title', StringType(), True),
         StructField('keywords', ArrayType(StringType()), True)
     ])
-    articles = [("Sam and Sam", []), ("Not Sam", ["keyword1", "keyword2"])]
+    articles = [("Sam and Sam", ["keyword1", "keyword2"]), ("", [])]
     df = spark.createDataFrame(data=articles, schema=spark_schema)
     df.write.format("io.weaviate.spark.Weaviate") \
         .option("scheme", "http") \
@@ -89,4 +90,54 @@ def test_string_arrays(spark: SparkSession, weaviate_client: weaviate.Client):
         .mode("append").save()
 
     weaviate_articles = weaviate_client.data_object.get(class_name="Article").get("objects")
+    weaviate_articles.sort(key=lambda x: x["properties"]["title"], reverse=True)
     assert len(weaviate_articles) == 2
+    assert weaviate_articles[0]["properties"]["title"] == "Sam and Sam"
+    assert weaviate_articles[0]["properties"]["keywords"] == articles[0][1]
+    assert weaviate_articles[1]["properties"]["title"] == ""
+    assert weaviate_articles[1]["properties"]["keywords"] == articles[1][1]
+
+
+def test_null_values(spark: SparkSession, weaviate_client: weaviate.Client):
+    article = {"class": "Article",
+               "properties": [
+                   {"name": "title", "dataType": ["string"]},
+                   {"name": "keywords", "dataType": ["string[]"]},
+                   {"name": "double", "dataType": ["number"]},
+                   {"name": "doubleArray", "dataType": ["number[]"]},
+                   {"name": "integer", "dataType": ["int"]},
+                   {"name": "integerArray", "dataType": ["int[]"]},
+                   {"name": "bool", "dataType": ["boolean"]},
+                   {"name": "date", "dataType": ["date"]},
+               ]}
+    weaviate_client.schema.create_class(article)
+
+    spark_schema = StructType([
+        StructField('title', StringType(), True),
+        StructField('keywords', ArrayType(StringType()), True),
+        StructField('double', DoubleType(), True),
+        StructField('doubleArray', ArrayType(DoubleType()), True),
+        StructField('integer', IntegerType(), True),
+        StructField('integerArray', ArrayType(IntegerType()), True),
+        StructField('bool', BooleanType(), True),
+        StructField('date', DateType(), True),
+    ])
+    articles = [(None, None, None, None, None, None, None, None)]
+    df = spark.createDataFrame(data=articles, schema=spark_schema)
+    df.write.format("io.weaviate.spark.Weaviate") \
+        .option("scheme", "http") \
+        .option("host", "localhost:8080") \
+        .option("className", "Article") \
+        .mode("append").save()
+
+    weaviate_articles = weaviate_client.data_object.get(class_name="Article").get("objects")
+    assert len(weaviate_articles) == 1
+    article = weaviate_articles[0]
+    assert article["properties"]["title"] == ""
+    assert article["properties"]["keywords"] == []
+    assert article["properties"]["double"] == 0.0
+    assert article["properties"]["doubleArray"] == []
+    assert article["properties"]["integer"] == 0
+    assert article["properties"]["integerArray"] == []
+    assert article["properties"]["bool"] == False
+    assert article["properties"]["date"] == "1970-01-01T00:00:00Z"
