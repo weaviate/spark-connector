ff1701a929f8916b7635c0d38c4793de392005bf
Fabrizio Castrotorres <fcastrotorres@chanzuckerberg.com>
2023-10-17 13:58:42 -0700

    feat: Enable writing with a consistency level

--- src/main/scala/Exceptions.scala
+++ src/main/scala/Exceptions.scala
@@ -5,3 +5,5 @@ case class SparkDataTypeNotSupported(s: String) extends Exception(s) {}
 case class WeaviateResultError(s: String) extends Exception(s) {}
 
 case class WeaviateClassNotFoundError(s: String) extends Exception(s) {}
+
+case class WeaviateOptionsError(s: String) extends Exception(s) {}
--- src/main/scala/WeaviateDataWriter.scala
+++ src/main/scala/WeaviateDataWriter.scala
@@ -24,8 +24,17 @@ case class WeaviateDataWriter(weaviateOptions: WeaviateOptions, schema: StructTy
 
   def writeBatch(retries: Int = weaviateOptions.retries): Unit = {
     if (batch.size == 0) return
+
+    val consistencyLevel = weaviateOptions.consistencyLevel
     val client = weaviateOptions.getClient()
-    val results = client.batch().objectsBatcher().withObjects(batch.values.toList: _*).run()
+
+    val results = if (consistencyLevel != "") {
+      logInfo(s"Writing using consistency level: ${consistencyLevel}")
+      client.batch().objectsBatcher().withObjects(batch.values.toList: _*).withConsistencyLevel(consistencyLevel).run()
+    } else {
+      client.batch().objectsBatcher().withObjects(batch.values.toList: _*).run()
+    }
+
     val IDs = batch.keys.toList
 
     if (results.hasErrors || results.getResult == null) {
